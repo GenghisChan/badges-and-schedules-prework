911241eb7b06ce12da0c013a82631d1b1e4c41d8
Christofer Chan <devchumbing@gmail.com>
2018-06-15 19:32:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 
 def assign_rooms(array)
   array.each_with_index.collect { |name, index|
-    "Hello, #{name}! You'll be assigned to room #{index + 1}"
+    "Hello, #{name}! You'll be assigned to room #{index + 1}!"
   }
 
 end
